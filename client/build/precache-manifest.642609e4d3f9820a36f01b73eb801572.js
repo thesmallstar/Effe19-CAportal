e7b350ae01e58e446c46df9d1b796ba8e20ffb8b
thesmallstar <manthan.surkar@gmail.com>
2019-08-22 00:48:31 +0530

    add build

---
+++ client/build/precache-manifest.642609e4d3f9820a36f01b73eb801572.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = [
+  {
+    "revision": "4e5f657226e65c348a11d12917eaf851",
+    "url": "/static/media/effe.4e5f6572.png"
+  },
+  {
+    "revision": "12359f330c57544f2560",
+    "url": "/static/css/main.639e7b01.chunk.css"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "171d363de9434abe5c53a6ea039fb451",
+    "url": "/static/media/ca.171d363d.jpg"
+  },
+  {
+    "revision": "95d0b1dd5871342111b4",
+    "url": "/static/js/2.95d0b1dd.chunk.js"
+  },
+  {
+    "revision": "863d3c2a1cdba3e4e415bcd0bee488ef",
+    "url": "/static/media/img2.863d3c2a.jpg"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  },
+  {
+    "revision": "12359f330c57544f2560",
+    "url": "/static/js/main.12359f33.chunk.js"
+  },
+  {
+    "revision": "9fcd4a71bbdcaad93749f07aeddd4479",
+    "url": "/static/media/img1.9fcd4a71.jpg"
+  },
+  {
+    "revision": "51877c3df3ffd965eac39524a6e7b93c",
+    "url": "/static/media/back.51877c3d.jpg"
+  },
+  {
+    "revision": "d6f8f2b86981a3e4456e17863ff971c9",
+    "url": "/static/media/37811295_2096965080374205_4625127831544791040_n.d6f8f2b8.jpeg"
+  },
+  {
+    "revision": "1c176b4b9800d1928bac1b1214dcfbc5",
+    "url": "/static/media/tickets.1c176b4b.jpg"
+  },
+  {
+    "revision": "fe96151bfe0b3c849dc384e9666d0db6",
+    "url": "/static/media/zebronics.fe96151b.jpg"
+  },
+  {
+    "revision": "95d0b1dd5871342111b4",
+    "url": "/static/css/2.74c9e1ec.chunk.css"
+  },
+  {
+    "revision": "d9376b5186e4f0562bc6af17d29dea07",
+    "url": "/index.html"
+  }
+];
