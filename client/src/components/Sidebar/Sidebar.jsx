8fe24d5afcba46c4db6c70f53df92219a6a4ed3c
thesmallstar <manthan.surkar@gmail.com>
2019-08-22 00:34:17 +0530

    minor changes

--- client/src/components/Sidebar/Sidebar.jsx
+++ client/src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import classNames from "classnames";
 import PropTypes from "prop-types";
-import { NavLink} from "react-router-dom";
+import { NavLink } from "react-router-dom";
 // @material-ui/core components
 import withStyles from "@material-ui/core/styles/withStyles";
 import Drawer from "@material-ui/core/Drawer";
@@ -29,153 +29,169 @@ const Sidebar = ({ ...props }) => {
         var activePro = " ";
         var listItemClasses;
 
-        if(prop.path!=='/login-page' && prop.path!=='/register-page' && prop.path!=='/viewTask')
-        {
-            
+        if (
+          prop.path !== "/login-page" &&
+          prop.path !== "/register-page" &&
+          prop.path !== "/viewTask"
+        ) {
           if (prop.path === "/logout") {
-            
             activePro = classes.activePro + " ";
             listItemClasses = classNames({
               [" " + classes[color]]: true
             });
 
             const whiteFontClasses = classNames({
-            [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
-          });
-          return (
-            <NavLink
-              to={'/'}
-              // to={prop.layout+prop.path}
-              className={activePro + classes.item}
-              activeClassName="active"
-              key={key}
-            >
-            
-              <ListItem button className={classes.itemLink + listItemClasses}>
-                {typeof prop.icon === "string" ? (
-                  <Icon
-                    className={classNames(classes.itemIcon, whiteFontClasses, {
-                      [classes.itemIconRTL]: props.rtlActive
+              [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
+            });
+            return (
+              <NavLink
+                to={"/"}
+                // to={prop.layout+prop.path}
+                className={activePro + classes.item}
+                activeClassName="active"
+                key={key}
+              >
+                <ListItem button className={classes.itemLink + listItemClasses}>
+                  {typeof prop.icon === "string" ? (
+                    <Icon
+                      className={classNames(
+                        classes.itemIcon,
+                        whiteFontClasses,
+                        {
+                          [classes.itemIconRTL]: props.rtlActive
+                        }
+                      )}
+                    >
+                      {prop.icon}
+                    </Icon>
+                  ) : (
+                    <prop.icon
+                      className={classNames(
+                        classes.itemIcon,
+                        whiteFontClasses,
+                        {
+                          [classes.itemIconRTL]: props.rtlActive
+                        }
+                      )}
+                    />
+                  )}
+                  <ListItemText
+                    primary={props.rtlActive ? prop.rtlName : prop.name}
+                    className={classNames(classes.itemText, whiteFontClasses, {
+                      [classes.itemTextRTL]: props.rtlActive
                     })}
-                  >
-                    {prop.icon}
-                  </Icon>
-                ) : (
-                  <prop.icon
-                    className={classNames(classes.itemIcon, whiteFontClasses, {
-                      [classes.itemIconRTL]: props.rtlActive
-                    })}
+                    disableTypography={true}
                   />
-                )}
-                <ListItemText
-                  primary={props.rtlActive ? prop.rtlName : prop.name}
-                  className={classNames(classes.itemText, whiteFontClasses, {
-                    [classes.itemTextRTL]: props.rtlActive
-                  })}
-                  disableTypography={true}
-                />
-              </ListItem>
-            </NavLink>
+                </ListItem>
+              </NavLink>
             );
-
-
-          } 
-          else if(prop.path === "/contactus")
-          {
-
+          } else if (prop.path === "/contactus") {
             listItemClasses = classNames({
               [" " + classes[color]]: activeRoute(prop.layout + prop.path)
             });
 
             const whiteFontClasses = classNames({
-            [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
-          });
-          return (
-            <a href="mailto:effervescence@iiita.ac.in" target='_blank'>
-            <ListItem button className={classes.itemLink + listItemClasses}>
-                {typeof prop.icon === "string" ? (
-                  <Icon
-                    className={classNames(classes.itemIcon, whiteFontClasses, {
-                      [classes.itemIconRTL]: props.rtlActive
+              [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
+            });
+            return (
+              <a href="mailto:effervescence@iiita.ac.in" target="_blank">
+                <ListItem button className={classes.itemLink + listItemClasses}>
+                  {typeof prop.icon === "string" ? (
+                    <Icon
+                      className={classNames(
+                        classes.itemIcon,
+                        whiteFontClasses,
+                        {
+                          [classes.itemIconRTL]: props.rtlActive
+                        }
+                      )}
+                    >
+                      {prop.icon}
+                    </Icon>
+                  ) : (
+                    <prop.icon
+                      className={classNames(
+                        classes.itemIcon,
+                        whiteFontClasses,
+                        {
+                          [classes.itemIconRTL]: props.rtlActive
+                        }
+                      )}
+                    />
+                  )}
+                  <ListItemText
+                    primary={props.rtlActive ? prop.rtlName : prop.name}
+                    className={classNames(classes.itemText, whiteFontClasses, {
+                      [classes.itemTextRTL]: props.rtlActive
                     })}
-                  >
-                    {prop.icon}
-                  </Icon>
-                ) : (
-                  <prop.icon
-                    className={classNames(classes.itemIcon, whiteFontClasses, {
-                      [classes.itemIconRTL]: props.rtlActive
-                    })}
+                    disableTypography={true}
                   />
-                )}
-                <ListItemText
-                  primary={props.rtlActive ? prop.rtlName : prop.name}
-                  className={classNames(classes.itemText, whiteFontClasses, {
-                    [classes.itemTextRTL]: props.rtlActive
-                  })}
-                  disableTypography={true}
-                />
-              </ListItem>
-            </a>
+                </ListItem>
+              </a>
             );
-          }
-          
-          else {
+          } else {
             listItemClasses = classNames({
               [" " + classes[color]]: activeRoute(prop.layout + prop.path)
             });
 
             const whiteFontClasses = classNames({
-            [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
-          });
-          return (
-            <NavLink
-              to={prop.layout + prop.path}
-              className={activePro + classes.item}
-              activeClassName="active"
-              key={key}
-            >
-            
-              <ListItem button className={classes.itemLink + listItemClasses}>
-                {typeof prop.icon === "string" ? (
-                  <Icon
-                    className={classNames(classes.itemIcon, whiteFontClasses, {
-                      [classes.itemIconRTL]: props.rtlActive
+              [" " + classes.whiteFont]: activeRoute(prop.layout + prop.path)
+            });
+            return (
+              <NavLink
+                to={prop.layout + prop.path}
+                className={activePro + classes.item}
+                activeClassName="active"
+                key={key}
+              >
+                <ListItem button className={classes.itemLink + listItemClasses}>
+                  {typeof prop.icon === "string" ? (
+                    <Icon
+                      className={classNames(
+                        classes.itemIcon,
+                        whiteFontClasses,
+                        {
+                          [classes.itemIconRTL]: props.rtlActive
+                        }
+                      )}
+                    >
+                      {prop.icon}
+                    </Icon>
+                  ) : (
+                    <prop.icon
+                      className={classNames(
+                        classes.itemIcon,
+                        whiteFontClasses,
+                        {
+                          [classes.itemIconRTL]: props.rtlActive
+                        }
+                      )}
+                    />
+                  )}
+                  <ListItemText
+                    primary={props.rtlActive ? prop.rtlName : prop.name}
+                    className={classNames(classes.itemText, whiteFontClasses, {
+                      [classes.itemTextRTL]: props.rtlActive
                     })}
-                  >
-                    {prop.icon}
-                  </Icon>
-                ) : (
-                  <prop.icon
-                    className={classNames(classes.itemIcon, whiteFontClasses, {
-                      [classes.itemIconRTL]: props.rtlActive
-                    })}
+                    disableTypography={true}
                   />
-                )}
-                <ListItemText
-                  primary={props.rtlActive ? prop.rtlName : prop.name}
-                  className={classNames(classes.itemText, whiteFontClasses, {
-                    [classes.itemTextRTL]: props.rtlActive
-                  })}
-                  disableTypography={true}
-                />
-              </ListItem>
-            </NavLink>
+                </ListItem>
+              </NavLink>
             );
           }
-      }})
-      }
+        }
+      })}
     </List>
   );
   var brand = (
     <div className={classes.logo}>
       <a
-        href="https://www.creative-tim.com"
+        href=""
         className={classNames(classes.logoLink, {
           [classes.logoLinkRTL]: props.rtlActive
         })}
-      > 
-        <div  className={classes.logoImage}>
+      >
+        <div className={classes.logoImage}>
           <img src={logo} alt="logo" className={classes.img} />
         </div>
         {logoText}
